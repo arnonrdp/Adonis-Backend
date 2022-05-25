a48d6d72643ab15b8faaaa0b5deec262f695fa32
Arnon <arnonrdp@gmail.com>
2022-05-25 13:51:00 -0300

    Níveis de permissão

--- database/migrations/1651701084622_users.ts
+++ database/migrations/1651701084622_users.ts
@@ -8,8 +8,8 @@ export default class UsersSchema extends BaseSchema {
       table.increments('id').primary()
       table.string('email', 255).notNullable()
       table.string('password', 180).notNullable()
+      table.enu('role', ['admin', 'normal']).notNullable().defaultTo('normal')
       table.string('remember_me_token').nullable()
-
       /**
        * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
        */
